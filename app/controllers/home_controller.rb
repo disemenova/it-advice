a13026b8ddbe57ab884911ed9290e29973bff54f
disemenova <di.semenovaa@gmail.com>
2023-11-27 20:03:07 +0300

    Верстка хедера

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   def index
+=begin
     if user_signed_in?
       redirect_to :edit_user_registration
     else
       redirect_to :new_user_registration
     end
+=end
+    render 'tests/gallery'
   end
 end
